9dafa4f29479328ecd9f238aa63e1e642d7aede1
andrewyturaldi <andrew.yturaldi2@gmail.com>
2024-07-18 23:27:48 -0500

    Multiple changes: article page spacing/styling, navbar font size, responsive design

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -26,7 +26,7 @@ function ArticlePage() {
                 <div className="article-page-container">
                     <p className="article-page-section">{articleData.section}</p>
                     <h2 className="article-page-title">{articleData.title}</h2>
-                    <p className="article-page-info">Author: {articleData.author} | {articleData.date}</p>
+                    <p className="article-page-info">Author: {articleData.author} &nbsp; <span className="stay-together">Date: {articleData.date}</span></p>
                     <img className="article-page-img" src={articleData.coverPhotoFull} alt={articleData.alt} width="1236" height="696"/>
                     {/* Render sanitized HTML content using dangerouslySetInnerHTML */}
                     <div className="article-content" dangerouslySetInnerHTML={{ __html: sanitizedContent }}/>
